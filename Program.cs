7a260d35854c5669345451604b7e52ff9f71772e
KessyRamos <kessyellenramos@gmail.com>
2021-09-22 15:46:37 -0300

    codigo inicial

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ArrayColection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] alunosAMS = new string[5] {
+                "Dani",
+                "Lucas",
+                "Ramon",
+                "Bruno",
+                "Sofia" };
+
+            Console.WriteLine("-------------");
+
+            Console.WriteLine(alunosAMS[3]);
+
+            Console.WriteLine("-------------");
+
+            alunosAMS[3] = "Antonio Junior";
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"Novo valor no index 3 -> {alunosAMS[3]}");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("---FOREACH---");
+
+            Console.WriteLine("");
+
+            foreach (string aluno in alunosAMS)
+            {
+                Console.WriteLine(aluno);
+            }
+
+            Console.WriteLine("");
+
+            Console.WriteLine("-----FOR-----");
+
+            Console.WriteLine("");
+
+            for (int i = 0; i < alunosAMS.Length; i++)
+            {
+                Console.WriteLine(alunosAMS[i]);
+            }
+            Console.WriteLine("");
+        }
+    }
+}
